53ea5e42485039bfe6819c8638c07204e936d3df
vkcldhkd <vkcldhkd@gmail.com>
2019-04-17 17:23:39 +0900

    add String.Swift

---
+++ String.swift
@@ -0,0 +1,24 @@
+//
+//  String.swift
+//
+//  Created by 1 on 15/10/2018.
+//  Copyright © 2018 Sung hyun. All rights reserved.
+//
+
+import UIKit
+
+extension String{    
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return NSAttributedString() }
+        do {
+            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            return NSAttributedString()
+        }
+    }
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
+}
+
+
